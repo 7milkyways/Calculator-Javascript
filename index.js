3cc4fb2d3c44db3a9935253a6be275b4f3565d45
Destiny <omoruyidestiny30@gmail.com>
2022-06-23 05:46:51 +0100

    task complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hello world");
+
+function calculator(a,b,operation){
+    let result = 0;
+    if(operation == "+"){
+       result = a + b;
+    }
+    else if( operation == "-"){
+       result = a - b;
+    }
+    else if( operation == "*"){
+        result = a * b;
+     }
+    else if( operation == "/"){
+       result = a / b;
+    }
+    else{
+        result = "operation is invalid: please check your operation"
+    }
+
+    return result;
+}
+
+
+console.log(calculator(1,2,));
